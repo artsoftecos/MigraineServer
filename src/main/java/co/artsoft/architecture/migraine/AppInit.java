020f7393805de3ae15c989acf9a2af6c73bcd4ff
Walter ALonso <wj.alonso@uniandes.edu.co>
2017-06-24 23:20:52 -0500

    cambios sobre el pom.xml

--- src/main/java/co/artsoft/architecture/migraine/AppInit.java
+++ src/main/java/co/artsoft/architecture/migraine/AppInit.java
@@ -1,13 +1,19 @@
 package co.artsoft.architecture.migraine;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
-public class AppInit {
+public class AppInit implements CommandLineRunner {
 
+	@Autowired
+    DataSource dataSource;
+	
     @RequestMapping("/")
     String home() {
         return "ArtSoft Rocks!";
@@ -15,6 +21,13 @@ public class AppInit {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(AppInit.class, args);
+    }
+    
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("DATASOURCE = " + dataSource);
+
     }
 
 }

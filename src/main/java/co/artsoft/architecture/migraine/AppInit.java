fe4af6494360a9c0850d01e20b0bbd58f8d6a1b7
Juan Paz <juan.paz@safetech.com.co>
2017-06-27 19:46:49 -0500

    Cambios para deploy#

--- src/main/java/co/artsoft/architecture/migraine/AppInit.java
+++ src/main/java/co/artsoft/architecture/migraine/AppInit.java
@@ -2,12 +2,20 @@ package co.artsoft.architecture.migraine;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.*;
+
 
 @SpringBootApplication
 @RestController
-public class AppInit {
+public class AppInit extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AppInit.class);
+    }
+	
     @RequestMapping("/")
     String home() {
         return "ArtSoft Rocks!";

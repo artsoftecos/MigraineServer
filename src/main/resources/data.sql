2868939e4afef40bd1ac588bc53adbed99acfedd
Walter ALonso <wj.alonso@uniandes.edu.co>
2017-06-29 11:34:33 -0500

    Se modifica la bd para data inicial y el application properties con los datos de la bd

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,135 +1,135 @@
 ----------------INSERTING FOOD DATA ----------------------------
-INSERT INTO dbmigraine.alimento (nombre)
+INSERT INTO ebdb.alimento (nombre)
 SELECT * FROM (SELECT 'Pollo') AS tmp
 WHERE NOT EXISTS (
-    SELECT nombre FROM dbmigraine.alimento WHERE nombre = 'Pollo'
+    SELECT nombre FROM ebdb.alimento WHERE nombre = 'Pollo'
 ) LIMIT 1;
 
-INSERT INTO dbmigraine.alimento (nombre)
+INSERT INTO ebdb.alimento (nombre)
 SELECT * FROM (SELECT 'Carne') AS tmp
 WHERE NOT EXISTS (
-    SELECT nombre FROM dbmigraine.alimento WHERE nombre = 'Carne'
+    SELECT nombre FROM ebdb.alimento WHERE nombre = 'Carne'
 ) LIMIT 1;
 
-INSERT INTO dbmigraine.alimento (nombre)
+INSERT INTO ebdb.alimento (nombre)
 SELECT * FROM (SELECT 'Cerdo') AS tmp
 WHERE NOT EXISTS (
-    SELECT nombre FROM dbmigraine.alimento WHERE nombre = 'Cerdo'
+    SELECT nombre FROM ebdb.alimento WHERE nombre = 'Cerdo'
 ) LIMIT 1;
 
-INSERT INTO dbmigraine.alimento (nombre)
+INSERT INTO ebdb.alimento (nombre)
 SELECT * FROM (SELECT 'Empanada') AS tmp
 WHERE NOT EXISTS (
-    SELECT nombre FROM dbmigraine.alimento WHERE nombre = 'Empanada'
+    SELECT nombre FROM ebdb.alimento WHERE nombre = 'Empanada'
 ) LIMIT 1;
 
-INSERT INTO dbmigraine.alimento (nombre)
+INSERT INTO ebdb.alimento (nombre)
 SELECT * FROM (SELECT 'Hamburguesa') AS tmp
 WHERE NOT EXISTS (
-    SELECT nombre FROM dbmigraine.alimento WHERE nombre = 'Hamburguesa'
+    SELECT nombre FROM ebdb.alimento WHERE nombre = 'Hamburguesa'
 ) LIMIT 1;
 
 ---------------- Location -------------------------------------
-INSERT INTO dbmigraine.localizacion (nombre)
+INSERT INTO ebdb.localizacion (nombre)
 SELECT * FROM (SELECT 'Frontal') AS tmp
 WHERE NOT EXISTS (
-    SELECT nombre FROM dbmigraine.localizacion WHERE nombre = 'Frontal'
+    SELECT nombre FROM ebdb.localizacion WHERE nombre = 'Frontal'
 ) LIMIT 1;
 
-INSERT INTO dbmigraine.localizacion (nombre)
+INSERT INTO ebdb.localizacion (nombre)
 SELECT * FROM (SELECT 'Lateral Izquierdo') AS tmp
 WHERE NOT EXISTS (
-    SELECT nombre FROM dbmigraine.localizacion WHERE nombre = 'Lateral Izquierdo'
+    SELECT nombre FROM ebdb.localizacion WHERE nombre = 'Lateral Izquierdo'
 ) LIMIT 1;
 
-INSERT INTO dbmigraine.localizacion (nombre)
+INSERT INTO ebdb.localizacion (nombre)
 SELECT * FROM (SELECT 'Lateral Derecho') AS tmp
 WHERE NOT EXISTS (
-    SELECT nombre FROM dbmigraine.localizacion WHERE nombre = 'Lateral Derecho'
+    SELECT nombre FROM ebdb.localizacion WHERE nombre = 'Lateral Derecho'
 ) LIMIT 1;
 
-INSERT INTO dbmigraine.localizacion (nombre)
+INSERT INTO ebdb.localizacion (nombre)
 SELECT * FROM (SELECT 'Superior') AS tmp
 WHERE NOT EXISTS (
-    SELECT nombre FROM dbmigraine.localizacion WHERE nombre = 'Superior'
+    SELECT nombre FROM ebdb.localizacion WHERE nombre = 'Superior'
 ) LIMIT 1;
 
-INSERT INTO dbmigraine.localizacion (nombre)
+INSERT INTO ebdb.localizacion (nombre)
 SELECT * FROM (SELECT 'Posterior') AS tmp
 WHERE NOT EXISTS (
-    SELECT nombre FROM dbmigraine.localizacion WHERE nombre = 'Posterior'
+    SELECT nombre FROM ebdb.localizacion WHERE nombre = 'Posterior'
 ) LIMIT 1;
 
 ------------------- Medicine -------------------------------------
-INSERT INTO dbmigraine.medicamento (nombre)
+INSERT INTO ebdb.medicamento (nombre)
 SELECT * FROM (SELECT 'Acetaminofen') AS tmp
 WHERE NOT EXISTS (
-    SELECT nombre FROM dbmigraine.medicamento WHERE nombre = 'Acetaminofen'
+    SELECT nombre FROM ebdb.medicamento WHERE nombre = 'Acetaminofen'
 ) LIMIT 1;
 
-INSERT INTO dbmigraine.medicamento (nombre)
+INSERT INTO ebdb.medicamento (nombre)
 SELECT * FROM (SELECT 'Dolex') AS tmp
 WHERE NOT EXISTS (
-    SELECT nombre FROM dbmigraine.medicamento WHERE nombre = 'Dolex'
+    SELECT nombre FROM ebdb.medicamento WHERE nombre = 'Dolex'
 ) LIMIT 1;
 
 ------------------- Physical activity ------------------------------
-INSERT INTO dbmigraine.actividad_fisica (nombre)
+INSERT INTO ebdb.actividad_fisica (nombre)
 SELECT * FROM (SELECT 'Caminata') AS tmp
 WHERE NOT EXISTS (
-    SELECT nombre FROM dbmigraine.actividad_fisica WHERE nombre = 'Caminata'
+    SELECT nombre FROM ebdb.actividad_fisica WHERE nombre = 'Caminata'
 ) LIMIT 1;
 
-INSERT INTO dbmigraine.actividad_fisica (nombre)
+INSERT INTO ebdb.actividad_fisica (nombre)
 SELECT * FROM (SELECT 'Natacion') AS tmp
 WHERE NOT EXISTS (
-    SELECT nombre FROM dbmigraine.actividad_fisica WHERE nombre = 'Natacion'
+    SELECT nombre FROM ebdb.actividad_fisica WHERE nombre = 'Natacion'
 ) LIMIT 1;
 
-INSERT INTO dbmigraine.actividad_fisica (nombre)
+INSERT INTO ebdb.actividad_fisica (nombre)
 SELECT * FROM (SELECT 'Ciclismo') AS tmp
 WHERE NOT EXISTS (
-    SELECT nombre FROM dbmigraine.actividad_fisica WHERE nombre = 'Ciclismo'
+    SELECT nombre FROM ebdb.actividad_fisica WHERE nombre = 'Ciclismo'
 ) LIMIT 1;
 
 ------------------- User type ------------------------------
-INSERT INTO dbmigraine.tipo_usuario (nombre)
+INSERT INTO ebdb.tipo_usuario (nombre)
 SELECT * FROM (SELECT 'Doctor') AS tmp
 WHERE NOT EXISTS (
-    SELECT nombre FROM dbmigraine.tipo_usuario WHERE nombre = 'Doctor'
+    SELECT nombre FROM ebdb.tipo_usuario WHERE nombre = 'Doctor'
 ) LIMIT 1;
 
-INSERT INTO dbmigraine.tipo_usuario (nombre)
+INSERT INTO ebdb.tipo_usuario (nombre)
 SELECT * FROM (SELECT 'Paciente') AS tmp
 WHERE NOT EXISTS (
-    SELECT nombre FROM dbmigraine.tipo_usuario WHERE nombre = 'Paciente'
+    SELECT nombre FROM ebdb.tipo_usuario WHERE nombre = 'Paciente'
 ) LIMIT 1;
 
 ---------------- Usuarios ----------------------------------
 --------Doctor------------
-INSERT INTO dbmigraine.usuario (numero_documento, nombre, id_tipo_usuario)
+INSERT INTO ebdb.usuario (numero_documento, nombre, id_tipo_usuario)
 SELECT * FROM (SELECT '1014207335', 'Doctor Patch Adams', 1) AS tmp
 WHERE NOT EXISTS (
-    SELECT numero_documento FROM dbmigraine.usuario WHERE numero_documento = '1014207335'
+    SELECT numero_documento FROM ebdb.usuario WHERE numero_documento = '1014207335'
 ) LIMIT 1;
 
-INSERT INTO dbmigraine.usuario (numero_documento, nombre, id_tipo_usuario)
+INSERT INTO ebdb.usuario (numero_documento, nombre, id_tipo_usuario)
 SELECT * FROM (SELECT '1014207334', 'Doctor House', 1) AS tmp
 WHERE NOT EXISTS (
-    SELECT numero_documento FROM dbmigraine.usuario WHERE numero_documento = '1014207334'
+    SELECT numero_documento FROM ebdb.usuario WHERE numero_documento = '1014207334'
 ) LIMIT 1;
 
 --------Paciente----------
-INSERT INTO dbmigraine.usuario (numero_documento, nombre, id_tipo_usuario)
+INSERT INTO ebdb.usuario (numero_documento, nombre, id_tipo_usuario)
 SELECT * FROM (SELECT '1014207336', 'Rebbeca Adler', 2) AS tmp
 WHERE NOT EXISTS (
-    SELECT numero_documento FROM dbmigraine.usuario WHERE numero_documento = '1014207336'
+    SELECT numero_documento FROM ebdb.usuario WHERE numero_documento = '1014207336'
 ) LIMIT 1;
 
 
 -------------- Episode -------------------------------------
-INSERT INTO dbmigraine.episodio (url_audio, fecha, nivel_dolor, patron_suenio, id_usuario)
+INSERT INTO ebdb.episodio (url_audio, fecha, nivel_dolor, patron_suenio, id_usuario)
 SELECT * FROM (SELECT '','2017-06-25', 4, '2 horas al día', '1014207336') AS tmp
 WHERE NOT EXISTS (
-    SELECT id_usuario FROM dbmigraine.episodio WHERE id_usuario = '1014207336'
+    SELECT id_usuario FROM ebdb.episodio WHERE id_usuario = '1014207336'
 ) LIMIT 1;
